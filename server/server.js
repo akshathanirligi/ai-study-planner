1569caf28f9fc951774922b77e6a7f04fb7de950
Akshatha <akshathanirligi03@gmail.com>
2026-07-21 16:20:00 +0530

    Add authentication route controllers and validation middleware

--- server/server.js
+++ server/server.js
@@ -5,10 +5,14 @@ require("dotenv").config({
     path: "./server/.env"
 });
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("Backend Running");
